dd8bc03f2893e0af4320f25c34c77a0ba2665aa8
Amber Hasan <amber.hasan@gmail.com>
2023-12-19 18:39:34 -0600

    The Main branch is how it is, and I am able to run SJT separately if I want, through the Main method. Saving this before I combine the two

--- src/Main.java
+++ src/Main.java
@@ -6,33 +6,55 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Random;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 public class Main {
-
-
     static int n = 4;
-    static int m = 2;
-    static int d = 3;
-    static int u = 1;
+    static int m = 3;
+    static int d = 1;
+    static int u = 0;
     static int N, Nmax = 0;
     static int sz;
     static int[][] P;
     static String indexFile;
     static boolean indexSearch;
 
+    // Global variables for SJT
+    static int[] direction;
+    static int[] sjtPermutation;
+    static int[] identityPermutation;
+    static int numCalls = 0;
+
     public static void main(String[] args) {
-        args = new String[] {String.valueOf(n), String.valueOf(m), String.valueOf(d), String.valueOf(u)};
+        identityPermutation = createIdentityPermutation(n);
+        sjtPermutation = identityPermutation;
+
+        // Initialize the direction array
+        direction = new int[n];
+        Arrays.fill(direction, -1); // Assuming -1 for left, 1 for right
+
+        //Call NextSJT 24 times
+        for (int i = 0; i < 24; i++) {
+            if (findNextSJTPermutation()) {
+                printPermutation();
+            }
+        }
+//        args = new String[] {String.valueOf(n), String.valueOf(m), String.valueOf(d), String.valueOf(u)};
 //        args = new String[] {String.valueOf(d), String.valueOf(u), "T(14,2,11)-5.txt"};
 
-        parseArgs(args);
+//        parseArgs(args);
+//
+//        if(indexSearch)
+//            IndexSearch();
+//        else
+//            FixedSearch();
+    }
 
-        if(indexSearch)
-            IndexSearch();
-        else
-            FixedSearch();
+    public static int[] createIdentityPermutation(int n) {
+        int[] identity = new int[n];
+        for (int i = 0; i < n; i++) {
+            identity[i] = i+1;
+        }
+        return identity;
     }
 
     public static void IndexSearch() {
@@ -215,6 +237,7 @@ public class Main {
             }
             hasNext = findNextLexicographicPermutation(data);
         }
+        System.out.println("Adding permutation to PA: " + Arrays.toString(data));
     }
 
     public static void swap(int[] data, int left, int right) {
@@ -280,6 +303,7 @@ public class Main {
                 }
             }
         }
+        System.out.println("Kendall tau distance between " + Arrays.toString(p1) + " and " + Arrays.toString(p2) + " is: " + res);
         return false;
     }
 
@@ -326,6 +350,46 @@ public class Main {
         return true;
     }
 
+    public static boolean findNextSJTPermutation() {
+        numCalls++;
+        if(numCalls == 1){
+            sjtPermutation = identityPermutation;
+            return true;
+        }
+        // Find the largest mobile element k
+        int k = -1, kIndex = -1;
+        for (int i = 0; i < sjtPermutation.length; i++) {
+            int adjacentNumIndex = i + direction[i];
+            if (adjacentNumIndex >= 0 && adjacentNumIndex < sjtPermutation.length) {
+                if ((direction[i] == -1 && sjtPermutation[i] > sjtPermutation[adjacentNumIndex]) ||
+                        (direction[i] == 1 && sjtPermutation[i] > sjtPermutation[adjacentNumIndex])) {
+                    if (k == -1 || sjtPermutation[i] > sjtPermutation[kIndex]) {
+                        k = sjtPermutation[i];
+                        kIndex = i;
+                    }
+                }
+            }
+        }
+
+        // No mobile element means this was the last permutation
+        if (k == -1) {
+            return false;
+        }
+
+        // Swap k with the element in its direction
+        int swapIndex = kIndex + direction[kIndex];
+        swap(sjtPermutation, kIndex, swapIndex);
+        swap(direction, kIndex, swapIndex);
+
+        // Reverse the direction of all elements larger than k
+        for (int i = 0; i < sjtPermutation.length; i++) {
+            if (sjtPermutation[i] > k) {
+                direction[i] = -direction[i];
+            }
+        }
+        return true;
+    }
+
     public static void parseArgs(String[] args) {
         if(args.length == 4) {
             n = Integer.parseInt(args[0]);
@@ -364,4 +428,12 @@ public class Main {
             System.exit(0);
         }
     }
+
+    private static void printPermutation() {
+        for (int i : sjtPermutation) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
 }
